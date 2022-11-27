9368df91b370230d94ae37ff329ded1d38bb328b
franciscovihdz <franciscovi_hdz@hotmail.com>
2022-11-26 20:23:27 -0600

    Create blockchain.py
    Primer fichero py

---
+++ Modulo 1 - Crear una Cadena de Bloques/blockchain.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 26 20:00:41 2022
+
+@author: franciscovicentehernandezantonio
+"""
+
+#Modulo 1 Cómo crear una cadena de bloques
